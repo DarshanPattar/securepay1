511ca3356ccd4dfd23e3bd565fd8f1da566e80ca
artmis22-03 <sricham2203@gmail.com>
2025-02-05 15:01:46 +0530

    models

--- execution/models.py
+++ execution/models.py
@@ -11,9 +11,28 @@ class Member(models.Model):
     unique_id=models.CharField(max_length=256)
     category=models.CharField(max_length=100,choices=cho,default='upto10k')
     password=models.CharField(max_length=100)
-
+    balance=models.IntegerField(default=0)
     def generateID(self):
         y=hashlib.sha256(str(self.id).encode()).hexdigest()
         self.unique_id=y
     
-    
+class Proposer(models.Model):
+    ch=[('pending','pending'),('accepted','accepted'),('rejected','rejected')]
+    fromid=models.CharField(max_length=256)
+    fname=models.CharField(max_length=100)
+    toidd=models.CharField(max_length=256)
+    toname=models.CharField(max_length=100)
+    amount=models.IntegerField()
+    status=models.CharField(max_length=100,choices=ch,default='pending')
+
+class Twoconfirms(models.Model):
+    ch=[('pending','pending'),('accepted','accepted'),('rejected','rejected')]
+    fromid=models.CharField(max_length=256)
+    toid=models.CharField(max_length=256)
+    amount=models.IntegerField()
+    imanager=models.CharField(max_length=100,choices=ch,default='pending')
+
+class Cmanager(models.Model):
+    memid=models.CharField(max_length=256)
+    memname=models.CharField(max_length=100)
+    amount=models.IntegerField()
